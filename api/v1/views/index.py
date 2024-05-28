bb09af7c5eacf0ea8f5c1fd7ce2b49bbde3673c5
Alaa Okair <alaaokair330@gmail.com>
2024-05-27 17:55:07 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,16 @@ from api.v1.views import app_views
 def ViewStatus():
     """returns a json message"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def viewStats():
+    """retrieves the number of each object by type"""
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
